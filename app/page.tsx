f6ff412222f195f7c8c43d913069c80323ed8b87
rejoanahmed <rejoanahmed8@gmail.com>
2023-10-07 21:53:01 +0800

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,23 @@
  * Warning: Opening too many live preview tabs will slow down performance.
  * We recommend closing them after you're done.
  */
-import React from 'react'
-import './global.css'
-import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   const pageId = 'Start'
 
   return (
-    <>
-      <Sidebar pageId={pageId} />
-      <div>a great start waiting to be copleted by this weekend</div>
-    </>
+    <div className='bg-slate-800'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi iure,
+      dicta minus laudantium autem eos sit cupiditate unde, eius numquam dolor
+      exercitationem aut magnam alias saepe. Inventore vero mollitia reiciendis
+      quos, nobis blanditiis voluptatum deserunt est nulla aut incidunt
+      doloribus ut, porro consequuntur quo repellendus natus autem! Doloribus
+      delectus similique natus dolore illum pariatur earum culpa non aut quas
+      tempora fugiat quae eveniet iure officia quidem aspernatur autem quisquam,
+      consectetur reiciendis vitae quia aliquid. Sunt quidem culpa nulla
+      mollitia recusandae ex nesciunt vel nam fugiat! Magnam nisi animi eos non
+      aspernatur excepturi nemo? Esse sed minus ullam soluta enim cumque?
+    </div>
   )
 }
 export default Home
